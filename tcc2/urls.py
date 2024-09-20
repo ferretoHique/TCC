57f698116a802374ef2cc025d4358aa645c4c64a
Henrique <hgferreto@gmail.com>
2024-09-20 19:55:10 -0300

    Adicao das telas

--- tcc2/urls.py
+++ tcc2/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('entrada', views.tela_entrada, name='tela_entrada'),
+    path('edicao', views.tela_edicao, name='tela_edicao'),
 ]
--- tcc2/views.py
+++ tcc2/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-def post_list(request):
-    return render(request, 'telas/tela_entrada.html', {})
+def tela_entrada(request):
+    return render(request, 'telas/tela_entrada.html', {'dado': 'aiin'})
+
+
+def tela_edicao(request):
+    return render(request, 'telas/tela_edicao.html', {'dado': 'aiin'})
